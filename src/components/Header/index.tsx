44e809d177ba2c1328e3238336b0027d6a02a7bc
Eduarda Carvalho <eduardacarvalho9833@gmail.com>
2022-05-06 14:00:55 -0300

    aula 2 finalizada

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logoImg from '../../assets/logo.svg' ;
+import { Container, Content } from './styles';
+
+interface headerProps{
+    onOpenNewTransactionModal: ( ()=> void);
+}
+
+export function Header({onOpenNewTransactionModal}: headerProps){
+    return(
+        <Container>
+            <Content>
+                <img src={logoImg} alt="dt money logo" />
+                <button type='button' onClick={onOpenNewTransactionModal}>
+                    Nova transação
+                </button>
+            </Content>
+        </Container>
+    )
+}
